9e2cb4c07172a6614f6b5ae97ccc439ae4b6c530
Dmitri Grabov <d.grabov@gmail.com>
2024-04-23 11:25:54 +0100

    Add example

--- src/cli/mod.ts
+++ src/cli/mod.ts
@@ -8,6 +8,10 @@ await new Command()
     'List of transformers to use',
     { required: true }
   )
+  .example(
+    'Generate MUI forms with RTK Query api client using Zod types',
+    'generate -t jsr:@schematicos/rtk-query jsr:@schematicos/mui-forms -s jsr:@schematicos/zod'
+  )
   .option('-s --typeSystem [module:string]', 'Type system to use', {
     default: 'jsr:@schematicos/zod'
   })
