581cbcf641d5bad2b21e644e10f2931413e3c9c6
Dmitri Grabov <d.grabov@gmail.com>
2024-04-23 15:46:02 +0100

    remove unused vendored libs

--- vendor/deno.land/x/configuration@0.2.0/import-map.ts
+++
@@ -1,5 +0,0 @@
-/** Represents an [import map](https://github.com/WICG/import-maps). */
-export interface ImportMap {
-	imports?: Record<string, string>;
-	scopes?: Record<string, Record<string, string>>;
-}
